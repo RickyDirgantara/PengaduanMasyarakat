28900e39f6b9346a9efec6e92f21276450075e37
RickyDirgantara <111202113268@mhs.dinus.ac.id>
2023-12-19 20:45:53 +0700

    updated

--- app/Views/complaintsUser.php
+++ app/Views/complaintsUser.php
@@ -50,19 +50,24 @@
     <div class="section-title text-center position-relative pb-3 mb-4 mx-auto" style="max-width: 600px;">
         <h1 class="mb-0">Cek status laporan anda disini</h1>
     </div>
-    <?php foreach ($complaints as $complaint) : ?>
-        <div class="testimonial-item bg-light my-4">
-            <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
-                <div class="ps-4">
-                    <h4 class="text-primary mb-1"><?= $complaint['JudulPengaduan']; ?></h4>
-                    <small class="text-uppercase"><?= $complaint['StatusPengaduan']; ?></small>
+
+    <?php if (empty($complaints)) : ?>
+        <p class="text-center">Laporan Belum Dibuat</p>
+    <?php else : ?>
+        <?php foreach ($complaints as $complaint) : ?>
+            <div class="testimonial-item bg-light my-4">
+                <div class="d-flex align-items-center border-bottom pt-5 pb-4 px-5">
+                    <div class="ps-4">
+                        <h4 class="text-primary mb-1"><?= $complaint['JudulPengaduan']; ?></h4>
+                        <small class="text-uppercase"><?= $complaint['StatusPengaduan']; ?></small>
+                    </div>
+                </div>
+                <div class="pt-4 pb-5 px-5">
+                    <?= $complaint['DeskripsiPengaduan']; ?>
                 </div>
             </div>
-            <div class="pt-4 pb-5 px-5">
-                <?= $complaint['DeskripsiPengaduan']; ?>
-            </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </div>
 
     
